8c43c98420ac8cae0109b4e401174c00b05d5441
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-04-20 22:15:01 -0700

    fix prettier

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,49 @@
-import EventEmitter from "events";
+import EventEmitter from 'events'
 
 function getDomainNameFromHostName(url: URL) {
-  const domainNameParts = url.hostname.split(".");
-  if (domainNameParts.length < 3) return url.hostname;
-  return domainNameParts[1] + "." + domainNameParts[2];
+  const domainNameParts = url.hostname.split('.')
+  if (domainNameParts.length < 3) return url.hostname
+  return domainNameParts[1] + '.' + domainNameParts[2]
 }
 
 export function domainsAreEqual(url: URL, otherUrl: URL) {
-  if (url.hostname === otherUrl.hostname) return true;
-  const urlDomain = getDomainNameFromHostName(url);
-  const otherUrlDomain = getDomainNameFromHostName(otherUrl);
-  if (urlDomain === otherUrlDomain) return true;
-  return false;
+  if (url.hostname === otherUrl.hostname) return true
+  const urlDomain = getDomainNameFromHostName(url)
+  const otherUrlDomain = getDomainNameFromHostName(otherUrl)
+  if (urlDomain === otherUrlDomain) return true
+  return false
 }
 
 export async function wait(ms: number) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(ms);
-    }, ms);
-  });
+      resolve(ms)
+    }, ms)
+  })
 }
 
 export interface HyperPlayAPI {
-  backendEvents: EventEmitter;
-  updatePopupInOverlay: (show: boolean) => void;
-  logError: (msg: string) => void;
-  logInfo: (msg: string) => void;
-  extensionProvider: any;
-  getMainWindowId: () => number;
-  openMetaMaskHomePage: () => void;
-  openMetaMaskPortfolioPage: () => void;
-  openMetaMaskSnapsPage: () => void;
-  setBadgeTextInRenderer: (text: string) => void;
-  openUrl: (url: string) => void;
-  captureException: (exception: any) => any;
-  configFolder: string;
-  appConfigFolder: string;
-  publicDir: string;
-  fixAsarPath: (origin: string) => string;
-  eventsToCloseMetaMaskPopupOn: string[];
-  appIconPath: string;
+  backendEvents: EventEmitter
+  updatePopupInOverlay: (show: boolean) => void
+  logError: (msg: string) => void
+  logInfo: (msg: string) => void
+  extensionProvider: any
+  getMainWindowId: () => number
+  openMetaMaskHomePage: () => void
+  openMetaMaskPortfolioPage: () => void
+  openMetaMaskSnapsPage: () => void
+  setBadgeTextInRenderer: (text: string) => void
+  openUrl: (url: string) => void
+  captureException: (exception: any) => any
+  configFolder: string
+  appConfigFolder: string
+  publicDir: string
+  fixAsarPath: (origin: string) => string
+  eventsToCloseMetaMaskPopupOn: string[]
+  appIconPath: string
 }
 
-export type OverlayAction = "ON" | "OFF" | "TOGGLE";
+export type OverlayAction = 'ON' | 'OFF' | 'TOGGLE'
 
 /**
  * @dev used by @hyperplay/overlay and @hyperplay/extension-helper
@@ -51,15 +51,15 @@ export type OverlayAction = "ON" | "OFF" | "TOGGLE";
  * @param action optionally overrides the overlay shown state instead of toggling
  */
 export async function toggleOverlay({
-  action = "TOGGLE",
-  api,
+  action = 'TOGGLE',
+  api
 }: {
-  action?: OverlayAction;
-  api: HyperPlayAPI;
+  action?: OverlayAction
+  api: HyperPlayAPI
 }) {
   try {
-    api.backendEvents.emit("overlayToggled", action);
+    api.backendEvents.emit('overlayToggled', action)
   } catch (err) {
-    console.log(`Error in hp overlay toggle ${err}`);
+    console.log(`Error in hp overlay toggle ${err}`)
   }
 }
