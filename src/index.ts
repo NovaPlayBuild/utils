c14df28459d06eaaa9c35785f72720ef9327490b
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-04-20 22:01:35 -0700

    pretty

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import EventEmitter from 'events'
+import EventEmitter from "events";
 
 function getDomainNameFromHostName(url: URL) {
   const domainNameParts = url.hostname.split(".");
@@ -23,27 +23,27 @@ export async function wait(ms: number) {
 }
 
 export interface HyperPlayAPI {
-  backendEvents: EventEmitter
-  updatePopupInOverlay: (show: boolean) => void
-  logError: (msg: string) => void
-  logInfo: (msg: string) => void
-  extensionProvider: any
-  getMainWindowId: ()=>number
-  openMetaMaskHomePage: ()=>void
-  openMetaMaskPortfolioPage: ()=>void
-  openMetaMaskSnapsPage: ()=>void
-  setBadgeTextInRenderer: (text: string)=>void
-  openUrl: (url: string)=>void
-  captureException: (exception: any) => any
-  configFolder: string
-  appConfigFolder: string
-  publicDir: string
-  fixAsarPath: (origin: string) => string
-  eventsToCloseMetaMaskPopupOn: string[]
-  appIconPath: string
+  backendEvents: EventEmitter;
+  updatePopupInOverlay: (show: boolean) => void;
+  logError: (msg: string) => void;
+  logInfo: (msg: string) => void;
+  extensionProvider: any;
+  getMainWindowId: () => number;
+  openMetaMaskHomePage: () => void;
+  openMetaMaskPortfolioPage: () => void;
+  openMetaMaskSnapsPage: () => void;
+  setBadgeTextInRenderer: (text: string) => void;
+  openUrl: (url: string) => void;
+  captureException: (exception: any) => any;
+  configFolder: string;
+  appConfigFolder: string;
+  publicDir: string;
+  fixAsarPath: (origin: string) => string;
+  eventsToCloseMetaMaskPopupOn: string[];
+  appIconPath: string;
 }
 
-export type OverlayAction = 'ON' | 'OFF' | 'TOGGLE'
+export type OverlayAction = "ON" | "OFF" | "TOGGLE";
 
 /**
  * @dev used by @hyperplay/overlay and @hyperplay/extension-helper
@@ -51,15 +51,15 @@ export type OverlayAction = 'ON' | 'OFF' | 'TOGGLE'
  * @param action optionally overrides the overlay shown state instead of toggling
  */
 export async function toggleOverlay({
-  action = 'TOGGLE',
-  api
+  action = "TOGGLE",
+  api,
 }: {
-  action?: OverlayAction
-  api: HyperPlayAPI
+  action?: OverlayAction;
+  api: HyperPlayAPI;
 }) {
   try {
-    api.backendEvents.emit('overlayToggled', action)
+    api.backendEvents.emit("overlayToggled", action);
   } catch (err) {
-    console.log(`Error in hp overlay toggle ${err}`)
+    console.log(`Error in hp overlay toggle ${err}`);
   }
 }
