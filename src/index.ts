304b07c7a0588d5323b77ce43d721933f5a935bf
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-05-23 13:54:15 -0700

    make get mm id async

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ export interface HyperPlayAPI {
       INITIAL_TOAST: NOTIFICATION_TYPE
     }
   }
-  getMetaMaskExtensionId: () => string
+  getMetaMaskExtensionId: () => Promise<string>
 }
 
 /**
