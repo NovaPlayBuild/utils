083779d3f004ae5992485f98b7c58424979fcbb3
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-05-28 23:15:36 -0700

    accept loss of precision

--- src/getAmount.test.ts
+++ src/getAmount.test.ts
@@ -16,17 +16,18 @@ describe('getAmount util', () => {
     expect(amt).toBe('1500000000000000000')
   })
 
-  test('get amount of fraction with 18 decimals', () => {
+  test('get amount of fraction with 77 decimals', () => {
+    /* eslint-disable-next-line @typescript-eslint/no-loss-of-precision */
     const amt = getAmount(1.1579208923731619542357098500868, 77).toString()
     expect(amt).toBe('1.1579208923731619e+77')
   })
 
-  test('get amount of fraction with 18 decimals', () => {
+  test('get amount of fraction with 0 decimals', () => {
     const amt = getAmount(1, 0).toString()
     expect(amt).toBe('1')
   })
 
-  test('get amount of fraction with 18 decimals', () => {
+  test('get amount of 0 with 0 decimals', () => {
     const amt = getAmount(0, 0).toString()
     expect(amt).toBe('0')
   })
