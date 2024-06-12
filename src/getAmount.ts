090965573cbd04db916c85e07580a01cb470572f
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-06-11 20:10:56 -0700

    do not use scientific notation for big number string

--- src/getAmount.ts
+++ src/getAmount.ts
@@ -1,6 +1,8 @@
 import BigNumber from 'bignumber.js'
 
 export function getAmount(amount: number, decimals: number) {
+  // ethers cannot interpret scientific notation strings so return the full string
+  BigNumber.config({ EXPONENTIAL_AT: 78 })
   const ten = new BigNumber(10)
   const exponent = BigNumber(decimals)
   const pow = ten.pow(exponent)
