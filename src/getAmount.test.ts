a683c0a7165fec4b3778512a361056d65c545e84
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-07-03 15:46:33 -0700

    fix loss of precision with amount as number

--- src/getAmount.test.ts
+++ src/getAmount.test.ts
@@ -2,35 +2,34 @@ import { getAmount, getDecimalNumberFromAmount } from './getAmount'
 
 describe('getAmount util', () => {
   test('get amount of 10 with 18 decimals', () => {
-    const amt = getAmount(10, 18).toString()
+    const amt = getAmount('10', 18).toString()
     expect(amt).toBe('10000000000000000000')
   })
 
   test('get amount with 6 decimals', () => {
-    const amt = getAmount(12312312, 6).toString()
+    const amt = getAmount('12312312', 6).toString()
     expect(amt).toBe('12312312000000')
   })
 
   test('get amount of fraction with 18 decimals', () => {
-    const amt = getAmount(1.5, 18).toString()
+    const amt = getAmount('1.5', 18).toString()
     expect(amt).toBe('1500000000000000000')
   })
 
-  test('get amount of fraction with 77 decimals', () => {
-    /* eslint-disable-next-line @typescript-eslint/no-loss-of-precision */
-    const amt = getAmount(1.1579208923731619542357098500868, 77).toString()
+  test('get amount of fraction with 77 decimals without loss of precision', () => {
+    const amt = getAmount('1.1579208923731619542357098500868', 77).toString()
     expect(amt).toBe(
-      '115792089237316190000000000000000000000000000000000000000000000000000000000000'
+      '115792089237316195423570985008680000000000000000000000000000000000000000000000'
     )
   })
 
   test('get amount of fraction with 0 decimals', () => {
-    const amt = getAmount(1, 0).toString()
+    const amt = getAmount('1', 0).toString()
     expect(amt).toBe('1')
   })
 
   test('get amount of 0 with 0 decimals', () => {
-    const amt = getAmount(0, 0).toString()
+    const amt = getAmount('0', 0).toString()
     expect(amt).toBe('0')
   })
 })
--- src/getAmount.ts
+++ src/getAmount.ts
@@ -2,7 +2,13 @@ import BigNumber from 'bignumber.js'
 
 // bigint does not support decimals/fractional numbers so we use BigNumber.js
 
-export function getAmount(amount: number, decimals: number) {
+/**
+ * @dev note that we pass a float string to avoid loss of precision when using JS Number
+ * @param amount float string (e.g. 1.001 WETH)
+ * @param decimals 10's exponent for smallest unit (i.e. 18 for WETH)
+ * @returns amount * 10 ^ decimals as a BigNumber
+ */
+export function getAmount(amount: string, decimals: number) {
   // ethers cannot interpret scientific notation strings so return the full string
   BigNumber.config({ EXPONENTIAL_AT: 78 })
   const ten = new BigNumber(10)
@@ -13,6 +19,12 @@ export function getAmount(amount: number, decimals: number) {
   return amountInDecimals
 }
 
+/**
+ * @dev note that we pass an integer string to avoid loss of precision when using JS Number
+ * @param amount int string (e.g. 1001000000000000000 WETH)
+ * @param decimals 10's exponent for smallest unit (i.e. 18 for WETH)
+ * @returns amount / 10 ^ decimals as a BigNumber
+ */
 export function getDecimalNumberFromAmount(amount: string, decimals: number) {
   // ethers cannot interpret scientific notation strings so return the full string
   BigNumber.config({ EXPONENTIAL_AT: 78 })
